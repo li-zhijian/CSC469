c108f84fd4bb9993979c63a565e1d5bbabf9e9eb
Abhinav Gupta <abhinav@Abhinavs-MacBook-Pro.local>
2013-02-10 02:09:17 -0500

    Added initial A1 files

---
+++ a1/inactive.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// http://www.cdf.toronto.edu/~csc469h/winter/assignments/a1/a1.shtml
+
+static uint64_t start = 0;
+
+void access_counter(unsigned *hi, unsigned *lo) {
+    asm volatile
+        ( "rdtsc; movl %%edx, %0; movl %%eax, %1" /* format string */
+        : "=r" (*hi), "=r" (*lo) /* output list */
+        : /* no inputs */
+        : "%edx", "%eax"); /* clobber list */
+}
+
+void start_counter() {
+    unsigned hi, lo;
+    access_counter(&hi, &lo);
+    start = ((uint64_t)hi << 32) | lo;
+}
+
+uint64_t get_counter() {
+    unsigned ncyc_hi, ncyc_lo;
+    access_counter(&ncyc_hi, &ncyc_lo);
+    return (((uint64_t)ncyc_hi << 32) | ncyc_lo) - start;
+}
+
+uint64_t inactive_periods(int num, uint64_t theshold, uint64_t *samples) {
+    // TODO
+}
+
+int main(int argc, char const *argv[]) {
+    start_counter();
+    printf("%llu\n", get_counter());
+}

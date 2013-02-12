b13648cafb66bb3be9750cf8aa1de9342ed18f1c
Zeeshan Qureshi <zee@zqureshi.in>
2013-02-11 19:58:46 -0500

    Determine CPU Frequency by counting CPU cycles for 1ms sleep intervals.

--- a1/trace.c
+++ a1/trace.c
@@ -4,7 +4,8 @@
 #include <stdint.h>
 #include <time.h>
 
-// http://www.cdf.toronto.edu/~csc469h/winter/assignments/a1/a1.shtml
+#define SLEEP_NSEC   1E6L  /* 1 millisecond = 10^6 nanoseconds */
+#define SLEEP_SAMPLES  5L
 
 static uint64_t start = 0;
 
@@ -16,13 +17,13 @@ static inline void access_counter(unsigned *hi, unsigned *lo) {
         : "%edx", "%eax"); /* clobber list */
 }
 
-void start_counter() {
+static inline void start_counter() {
     unsigned hi, lo;
     access_counter(&hi, &lo);
     start = ((uint64_t)hi << 32) | lo;
 }
 
-uint64_t get_counter() {
+static inline uint64_t get_counter() {
     unsigned ncyc_hi, ncyc_lo;
     access_counter(&ncyc_hi, &ncyc_lo);
     return (((uint64_t)ncyc_hi << 32) | ncyc_lo) - start;
@@ -34,7 +35,37 @@ uint64_t inactive_periods(int num, uint64_t theshold, uint64_t *samples) {
 }
 
 int main(int argc, char const *argv[]) {
+    /* Get clock resolution */
     struct timespec res;
     clock_getres(CLOCK_REALTIME, &res);
+
+    /* Get CPU frequency by sleeping for SLEEP_SAMPLES times */
+    struct timespec sleepTime = { 0, SLEEP_NSEC };
+    uint64_t sleepSamples[SLEEP_SAMPLES];
+    uint64_t start = 0, current = 0;
+
+    /**
+     *  As long as we don't go over 10 seconds of combined sleep time
+     *  this shouldn't overflow.
+     */
+    uint64_t cyclePerMSec = 0;
+
+    start_counter();
+    for(uint64_t i = 0; i < SLEEP_SAMPLES;) {
+        __asm__ volatile ( "cpuid;" );
+        /* do some busy waiting */
+        for(uint64_t j = 0; j < 1E8; j++);
+
+        start = get_counter();
+        if(nanosleep(&sleepTime, NULL) == 0) {
+            current = get_counter();
+            sleepSamples[i++] = current - start;
+            cyclePerMSec += sleepSamples[i-1];
+        }
+    }
+    cyclePerMSec = cyclePerMSec / SLEEP_SAMPLES;
+
     printf("Clock Resolution: %lds %ldns\n", res.tv_sec, res.tv_nsec);
+    printf("Clock Speed: %.2Lf GHz\n", cyclePerMSec / SLEEP_NSEC);
+
 }

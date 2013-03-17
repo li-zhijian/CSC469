ee5891faa6fe23c4b4190902979d1f58d3835361
Zeeshan Qureshi <zee@zqureshi.in>
2013-03-17 18:58:16 -0400

    Add test for number of processors.

--- a2/tests/hoard.cc
+++ a2/tests/hoard.cc
@@ -3,8 +3,15 @@
 #include <unistd.h>
 #include <gtest/gtest.h>
 
+extern "C" {
+#include "mm_thread.h"
 #include "debug.h"
+}
 
 TEST(Hoard, PageSize) {
     ASSERT_EQ(4096, getpagesize());
+}
+
+TEST(Hoard, Processors) {
+    TRACE("# of Processors: %d", getNumProcessors());
 }

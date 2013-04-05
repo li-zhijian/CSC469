edd651ef88d165742bfcc6d99974bc78bdd88166
Zeeshan Qureshi <zee@zqureshi.in>
2013-04-05 06:32:54 -0400

    Switch to MEMBER_KEEP_ALIVE for heartbeat.

--- a3/client_main.c
+++ a3/client_main.c
@@ -417,16 +417,10 @@ int handle_heartbeat_req() {
     char res[MAX_MSG_LEN];
 
     /**
-     * Using ROOM_LIST_REQUEST instead of MEMBER_KEEP_ALIVE, because
-     * the server doesn't handle MEMBER_KEEP_ALIVE and doesn't return anything.
-     * ROOM_LIST_REQUEST returns some data which verifies that the communication
-     * channel is working in both directions.
-     *
-     * This would not be efficient on a production environment because a ROOM_LIST
-     * request could potentially contain thousands of room names, thus over there
-     * using a dedicated heartbeat signal would be the right choice.
+     * Send a heartbeat to the server at a fixed interval to make sure
+     * we can still connect and communicate.
      */
-    if(send_control_msg(ROOM_LIST_REQUEST, NULL, 0, res) <= 0) {
+    if(send_control_msg(MEMBER_KEEP_ALIVE, NULL, 0, res) < 0) {
         return NETWORK_ERROR;
     }
 
